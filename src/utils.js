64c80c3e85f33674b0e76173c3b894b52b54832d
Franz-Art-L <francislandia@icloud.com>
2022-05-21 00:32:32 +0800

    one more commit before I will run build for this to be deployed

--- src/utils.js
+++ src/utils.js
@@ -9,10 +9,12 @@ export const json = response => response.json();
 
 export const Footer = () => {
     return(
-        <div className="py-2 my-4 text-center" style={{color: 'white'}}>
+        <div className="my-4 text-center" style={{color: 'white'}}>
+             <p className="text-center">API powered by<a href="https://omdbapi.com/" target="_blank"
+      rel="noopener noreferrer"> www.omdbapi.com</a></p>
              <p>ReactJs practice by: </p>
-
              <p><a href="https://confident-murdock-8e5bba.netlify.app/" target="_blank" rel="noopener noreferrer">Francis Artemio Landia</a></p>
+             <p><a href="https://www.altcademy.com/" target="_blank" rel="noopener noreferrer">Altcademy</a> Student</p>
              <span>2022</span>
         </div>
     );
